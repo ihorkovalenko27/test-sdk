61ff3b0ad0145c03e8fb33f474c15b0c62ebc149
Ihor Kovalenko <kovalyan27@gmail.com>
2023-02-13 19:58:57 +0200

    Refactor product model

--- src/models/Product.js
+++ src/models/Product.js
@@ -8,19 +8,23 @@ class Product {
 
   validateBody() {
     if (typeof this.title !== "string" || this.title === "") {
-      throw new Error("Product title field must be a string");
+      throw new Error("Title field must to be a string and not empty!");
     }
     if (typeof this.description !== "string" || this.description === "") {
-      throw new Error("Product description field must be a string");
+      throw new Error(
+        "Product description field must to be a string and not empty!"
+      );
     }
     if (typeof this.price !== "number" || this.price < 1) {
-      throw new Error("Product price field must be a number");
+      throw new Error(
+        "Product price field must be a number and more than zero"
+      );
     }
   }
 
   validateID() {
     if (typeof this.id !== "string" || this.id === "") {
-      throw new Error("Product id field must be a string");
+      throw new Error("Product id field must to be a string and not empty!");
     }
   }
 }
