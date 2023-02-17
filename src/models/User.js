e5ff776ce0f18068d4ea725f3aa255a71e976422
Ihor Kovalenko <kovalyan27@gmail.com>
2023-02-17 12:32:32 +0200

    Add validation

--- src/models/User.js
+++ src/models/User.js
@@ -1,16 +1,9 @@
+import { validation } from "../utils/validationUtil.js";
+
 class User {
   constructor({ username, password }) {
-    this.username = username;
-    this.password = password;
-  }
-
-  validate() {
-    if (typeof this.username !== "string" || this.username === "") {
-      throw new Error("Username field must to be a string and not empty!");
-    }
-    if (typeof this.password !== "string" || this.password === "") {
-      throw new Error("Password field must to be a string and not empty!");
-    }
+    this.username = validation(username, "string");
+    this.password = validation(password, "string");
   }
 }
 
