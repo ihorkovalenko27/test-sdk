9ed8868db53d09988e8d2e0892a409045279c0a2
Ihor Kovalenko <kovalyan27@gmail.com>
2023-02-17 15:46:19 +0200

    Refactor utils

--- src/utils/validationUtil.js
+++ src/utils/validationUtil.js
@@ -1,10 +1,7 @@
 export const validation = (value, type) => {
-  if (!value) {
-    return `${value} is empty`;
-  }
-  if (typeof value === type) {
-    return value;
-  }
+  if (!value) return `Value is empty`;
+
+  if (typeof value === type) return value;
 
   throw new TypeError(
     `Change ${value} type to: ${type}, cause you set type ${typeof value}`
